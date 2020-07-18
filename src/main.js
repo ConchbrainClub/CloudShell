568be6d3f69ff4e9c892776e82a2b27d824416e1
lixinyang123 <lixinyangemil@outlook.com>
2020-07-19 03:50:55 +0800

    新增延迟容器生命周期和自动回收容器功能

--- src/main.js
+++ src/main.js
@@ -1,13 +1,62 @@
 var http = require("http");
+var url = require("url");
 var ttyd = require("./ttyd");
 
 http.createServer((req,res)=>{
-    ttyd.run((id)=>{
-        if(id){
-            res.end(id);
-        }
-        res.end("error");
-    });
+
+    var path = url.parse(req.url).pathname;
+
+    switch(path){
+        case "/":
+            ttyd.run((id)=>{
+                if(id){
+                    res.end(id);
+                }
+                else{
+                    res.end("error");
+                }
+
+                ttyd.showStatus();
+            });
+
+            break;
+
+        case "/kill":
+            var containerId = url.parse(req.url).query;
+            ttyd.kill(containerId,(flag)=>{
+                if(flag){
+                    res.end("kill container "+containerId);
+                }
+                else{
+                    res.end("kill container defeat");
+                }
+
+                ttyd.showStatus();
+            })
+
+            break;
+
+        case "/delay":
+            var containerId = url.parse(req.url).query;
+
+            ttyd.delayedLife(containerId,(flag)=>{
+                if(flag){
+                    res.end("delay successful");
+                }
+                else{
+                    res.end("dealy defeat");
+                }
+            });
+
+            ttyd.showStatus();
+            break;
+
+        default:
+            res.statusCode = 404;
+            res.end();
+    }
+
+    
 }).listen(8080,()=>{
     console.log("server run at http://localhost:8080");
 });
--- src/ttyd.js
+++ src/ttyd.js
@@ -1,14 +1,22 @@
-const child_process = require("child_process");
+var child_process = require("child_process");
 
 var containers,usefulPorts;
 
+function container (id,port,endTime){
+    this.id = id;
+    this.port = port;
+    this.endTime = endTime;
+}
+
 function run(callback){
+
+    var port = usefulPorts.shift();
 
     var cmd = "docker run --rm -d -p " + port + ":7681/tcp tsl0922/ttyd:latest";
     child_process.exec(cmd,(error,stdout,stderr)=>{
-        if(!error||!stderr){
+        if(!error || !stderr){
             //存储容器id
-            containers.push(stdout);
+            containers.push(new container(stdout,port,new Date().getTime() + 1000 * 60));
 
             callback(stdout);
         }
@@ -18,16 +26,65 @@ function run(callback){
     });
 }
 
-function checkPortUseful(port,callback){
-    var cmd = "lsof -i:" + port;
-    child_process.exec(cmd,(error,stdout,stderr)=>{
-        if(!stdout){
-            callback(true);
-        }
-        else{
-            callback(false);
+function kill(containerId,callback){
+
+    var cmd = "docker rm -f " + containerId;
+    
+    containers.forEach(container => {
+        if(container.id.includes(containerId)){
+            child_process.exec(cmd,(error,stdout,stderr)=>{
+                if(!error || !stderr){
+                    //容器列表中移除
+                    containers.splice(containers.indexOf(container),1);
+                    //回收端口
+                    usefulPorts.push(container.cport);
+
+                    callback(stdout);
+                }
+                else{
+                    callback(undefined);
+                }
+            });
         }
     });
+}
+
+function showStatus(){
+    console.log("Containers " + containers.length);
+    console.log("UsefulPorts " + usefulPorts.length);
+}
+
+//延长容器声明周期
+function delayedLife(containerId,callback){
+    containers.forEach(container => {
+        if(container.id.includes(containerId)){
+            container.endTime = new Date().getTime() + 1000 * 60;
+            callback(true);
+        }
+    });
+    callback(false);
+}
+
+//自动回收过期容器
+function autoRecycling(){
+    containers.forEach(container => {
+        if(new Date().getTime() > container.endTime){
+            kill(container.id,(flag)=>{
+                if(flag){
+                    console.log("recycling container " + container.id);
+                }
+                else{
+                    console.log("recycling container defeat");
+                }
+                showStatus();
+            });
+        }
+        else{
+            console.log("have no container need recycling");
+        }
+    });
+
+    setTimeout(autoRecycling,1000*10);
 }
 
 function init(){
@@ -37,16 +94,13 @@ function init(){
 
     //初始化20个可用端口
     for(var i=7681;i<=7700;i++){
-        checkPortUseful(i,(flag)=>{
-            console.log(flag);
-            if(flag){
-                usefulPorts.push(i);
-            }
-        });
+        usefulPorts.push(i);
     }
 
+    autoRecycling();
+
     module.exports = {
-        run
+        run,kill,showStatus,delayedLife
     }
 }
 
