76927ac23079f9a7b2ca7f7d0d3346dd2557bfe6
lixinyang123 <lixinyangemil@outlook.com>
2020-07-18 12:24:57 +0800

    移除依赖

--- test/main.js
+++
@@ -1,7 +0,0 @@
-var Docker = require('dockerode');
-
-var docker = new Docker();
-
-docker.run('ubuntu', ['bash', '-c', 'uname -a'], process.stdout, function (err, data, container) {
-	  console.log(container);
-});
