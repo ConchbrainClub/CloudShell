05f630f3edb0c641c4937f0afc28ea9a82ce484d
root <root@DESKTOP-L76UIJK.localdomain>
2020-07-28 19:27:18 +0800

    新增archlinux支持

--- container/build.sh
+++ container/build.sh
@@ -1,3 +1,4 @@
 # docker build -t cloudshell:ubuntu ./ubuntu
 # docker build -t cloudshell:centos ./centos
 # docker build -t cloudshell:debian ./debian
+docker build -t cloudshell:archlinux ./archlinux
