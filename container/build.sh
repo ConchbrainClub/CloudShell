1a3b1d6b156511ca5090c201d1d1d4aa2062bed2
root <root@DESKTOP-L76UIJK.localdomain>
2020-07-29 18:41:38 +0800

    新增一些发行版本

--- container/build.sh
+++ container/build.sh
@@ -1,4 +1,6 @@
-# docker build -t cloudshell:ubuntu ./ubuntu
-# docker build -t cloudshell:centos ./centos
-# docker build -t cloudshell:debian ./debian
+docker build -t cloudshell:ubuntu ./ubuntu
+docker build -t cloudshell:centos ./centos
+docker build -t cloudshell:debian ./debian
+docker build -t cloudshell:alpine ./alpine
 docker build -t cloudshell:archlinux ./archlinux
+docker build -t cloudshell:kali ./kali
