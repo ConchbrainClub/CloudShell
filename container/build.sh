0967f616f664eb0125446b8028252dd008cd32dd
lixinyang123 <lixinyangemil@outlook.com>
2020-09-04 13:10:30 +0800

    统一镜像名称

--- container/build.sh
+++ container/build.sh
@@ -1,8 +1,8 @@
-docker build -t cloudshell:ubuntu ./ubuntu
-docker build -t cloudshell:centos ./centos
-docker build -t cloudshell:debian ./debian
-docker build -t cloudshell:alpine ./alpine
-docker build -t cloudshell:archlinux ./archlinux
-docker build -t cloudshell:kali ./kali
-docker build -t cloudshell:fedora ./fedora
-docker build -t cloudshell:opensuse ./opensuse
+docker build -t lixinyang/cloudshell:ubuntu ./ubuntu
+docker build -t lixinyang/cloudshell:centos ./centos
+docker build -t lixinyang/cloudshell:debian ./debian
+docker build -t lixinyang/cloudshell:alpine ./alpine
+docker build -t lixinyang/cloudshell:archlinux ./archlinux
+docker build -t lixinyang/cloudshell:kali ./kali
+docker build -t lixinyang/cloudshell:fedora ./fedora
+docker build -t lixinyang/cloudshell:opensuse ./opensuse
