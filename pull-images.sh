bbd65640398a9b0c1ab3aca65576ab8ce59072e2
lixinyang123 <lixinyangemil@outlook.com>
2023-05-04 22:05:32 +0800

    add some scripts

---
+++ pull-images.sh
@@ -0,0 +1,21 @@
+images=(latest ubuntu centos debian alpine archlinux kali fedora opensuse)
+
+for item in ${images[@]};
+do
+    echo docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/cloudshell:${item}
+    docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/cloudshell:${item}
+done
+
+for item in ${images[@]};
+do
+    docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/cloudshell:${item} lixinyang/cloudshell:${item}
+done
+
+for item in ${images[@]};
+do
+    docker rmi registry.cn-shenzhen.aliyuncs.com/lllxy/cloudshell:${item}
+done
+
+docker images  | grep none | awk '{print $3}' | xargs docker rmi
+
+echo "Pull image from Aliyun was successful!"
